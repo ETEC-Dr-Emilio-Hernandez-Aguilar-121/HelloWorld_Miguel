5c92bd17cb0a641e57a6d876f9388eabfe707ed7
Miguel <Miguel@Miguel-DESKTOP>
2021-02-20 00:10:11 -0200

    Commit do miguel dia 19/02

---
+++ src/pct/Ola.java
@@ -0,0 +1,12 @@
+
+package pct;
+
+import javax.swing.JOptionPane;
+
+public class Ola {
+    public static void main(String[] args){
+        //escrever boa tarde à todos 
+        //System.out.println("batarde");
+        JOptionPane.showMessageDialog(null, "Muito Boa Noite");
+    }        
+}
